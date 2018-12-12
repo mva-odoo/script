c7008a4e014f7160edafc502ccc8463ed02e8c75
mva-odoo <mva@odoo.com>
2018-12-12 12:08:38 +0100

    ADD script

---
+++ odoo-version.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+#Switch odoo version
+cd /odoo/enterprise && git checkout $1 && git pull && find . -name \*.pyc -delete && git clean -df
+cd /odoo/design-themes && git checkout $1 && git pull && find . -name \*.pyc -delete && git clean -df
+cd /odoo/odoo && git checkout $1 && git pull && find . -name \*.pyc -delete && git clean -df
+cd /odoo/internal && git pull && find . -name \*.pyc -delete && git clean -df
