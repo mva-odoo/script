00810febb8b1b5446145108c8114e8a5e66df669
mva-odoo <mva@odoo.com>
2019-03-27 09:37:27 +0100

    comment internal line

--- odoo-version.sh
+++ odoo-version.sh
@@ -3,4 +3,4 @@
 cd /odoo/enterprise && git checkout $1 && git pull && find . -name \*.pyc -delete && git clean -df
 cd /odoo/design-themes && git checkout $1 && git pull && find . -name \*.pyc -delete && git clean -df
 cd /odoo/odoo && git checkout $1 && git pull && find . -name \*.pyc -delete && git clean -df
-cd /odoo/internal && git pull && find . -name \*.pyc -delete && git clean -df
+#cd /odoo/internal && git pull && find . -name \*.pyc -delete && git clean -df
