8ce6537bcb9fbc2e7fba7c03606034ffe08f639d
mva-odoo <mva@odoo.com>
2019-03-27 09:40:04 +0100

    add config i3 and fish

---
+++ config/fish/config.fish
@@ -0,0 +1,13 @@
+function fish_prompt
+    powerline-shell --shell bare $status
+end
+
+set fish_greeting
+
+alias odoo-clean='/home/odoo/script/odoo-clean.sh'
+alias odoo='/home/odoo/script/odoo-version.sh'
+alias flamegraph='/home/odoo/Perf/FlameGraph/flamegraph.pl'
+alias kbdbacklight='/home/odoo/script/backlight.sh'
+alias cdodoo='cd /odoo/odoo'
+
+#ssh-add /home/odoo/.ssh/id_rsa
